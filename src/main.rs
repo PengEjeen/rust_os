8496031fa2118424a32c437e28dfc92da17fb7a1
root <root@siena.asia-northeast3-a.c.labb-377308.internal>
2023-03-02 06:15:19 +0000

    add new

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![no_std] //don't link with rust standart library
+#![no_main] //don't use main fn
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+
+//pub - public in mother module 
+//extern "C" use rust instead of C
+pub extern "C" fn _start() -> ! {
+    //like main this fn is startpoint
+    loop{}
+}
+
+//panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop{}
+}
